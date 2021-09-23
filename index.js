b9f668062b45032749b72a8ec50797a3a36239ee
Franklyn Colon <franklyncolon6@gmail.com>
2021-09-23 19:45:26 -0400

    commit tree c78dc6d96574689daa26f733345fb5857807f121

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber) {
+  if (someNumber === 199) {
+    return "This one is on me!";
+  }
+  if (someNumber === 2001) {
+    return "I will gladly take your thirty bucks.";
+  }
+  if (someNumber === 2501) {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(generous) {
+  switch (generous) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
